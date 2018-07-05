5c3bef805848c6d10d757900907d37c7def8d46c
koekaew <naruto14789@hotmail.com>
2018-07-05 11:47:27 +0700

    kaew ch

--- src/pages/main/main.ts
+++ src/pages/main/main.ts
@@ -14,22 +14,49 @@ import { AlertController } from 'ionic-angular';
 @Component({
   selector: 'page-main',
   templateUrl: 'main.html',
-  
+
 })
 export class MainPage {
+  items;
+  item1
 
-  constructor(public alerCtrl: AlertController,public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public alerCtrl: AlertController, public navCtrl: NavController, public navParams: NavParams) {
+    this.initializeItems();
   }
 
+  initializeItems() {
+    this.items = [
+      '111/1',
+      '112/1'
+    ];
+
+  }
+
+  getItems(ev) {
+    // Reset items back to all of the items
+    this.initializeItems();
+
+    // set val to the value of the ev target
+    var val = ev.target.value;
+
+    // if the value is an empty string don't filter the items
+    if (val && val.trim() != '') {
+      this.items = this.items.filter((item) => {
+        return (item.toLowerCase().indexOf(val.toLowerCase()) > -1);
+      })
+    }
+  }
   ionViewDidLoad() {
     console.log('ionViewDidLoad MainPage');
   }
-  generateSN1(){
+  generateSN1() {
     this.navCtrl.push(Sn1Page);
   }
   doConfirm() {
     let confirm = this.alerCtrl.create({
-      title: 'คุณต้องการบันทึกใช่หรือไม่?',
+      title: 'ปัญหาการเข้าพบ',
+      message: 'การเข้าพบครั้งที่ 1 <br> <b>ไม่พบผู้อยู่อาศัย</b> <br> การเข้าพบครั้งที่ 2 <br> <b>ไม่พบผู้อยู่อาศัย</b> <br> การเข้าพบครั้งที่ 3 <br> <b>พบผู้อยู่อาศัย<b>',
+      
       buttons: [
         {
           text: 'ยกเลิก',
